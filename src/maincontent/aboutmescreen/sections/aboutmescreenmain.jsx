fb2f9c7f0e32f6834ff7c200231033615678ca32
natbldavid <natbldavid@gmailcom>
2025-12-24 19:03:37 +0000

    Fixing all problems...hopefully

--- src/maincontent/aboutmescreen/sections/aboutmescreenmain.jsx
+++ src/maincontent/aboutmescreen/sections/aboutmescreenmain.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { basePath } from "@/lib/basePath"; // adjust path if needed
 
 function SparkleIcon({ className }) {
   return (
